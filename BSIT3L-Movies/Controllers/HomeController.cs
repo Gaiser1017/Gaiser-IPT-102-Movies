94d0afdb23cf9987b2a8c36b678ed57974c52b5f
PC <PC@DESKTOP-DTBK315>
2023-10-14 20:48:24 +0800

    Update

--- BSIT3L-Movies/Controllers/HomeController.cs
+++ BSIT3L-Movies/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using BSIT3L_Movies.Models;
 using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
 
 namespace BSIT3L_Movies.Controllers;
 
@@ -15,9 +16,22 @@ public class HomeController : Controller
         _logger = logger;
         _movies = new List<MovieViewModel>
         {
-            new MovieViewModel { Id = 1, Name = "Titanic", Rating = "5", ReleaseYear = 1997, Genre = "Romance/Drama" },
-            new MovieViewModel { Id = 2, Name = "Inception", Rating = "4", ReleaseYear = 2010, Genre = "Science Fiction/Thriller" },
-            new MovieViewModel { Id = 3, Name = "The Shawshank Redemption", Rating = "5", ReleaseYear = 1994, Genre = "Drama" },
+           new MovieViewModel { Id = 1, Name = "Iron Man", Rating = "1", Image = "css/picture/Pic1.jpg", Details = "After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil.\r\n" },
+           new MovieViewModel { Id = 2, Name = "Thor", Rating = "1", Image = "css/picture/pic2.jpg", Details = "" },
+           new MovieViewModel { Id = 3, Name = "Captain America: The first Avenger", Rating = "1", Image = "css/picture/pic3.jpg", Details = "" },
+           new MovieViewModel { Id = 4, Name = "The Avengers", Rating = "1", Image = "css/picture/pic4.jpg", Details = "" },
+           new MovieViewModel { Id = 5, Name = "Thor the Dark World", Rating = "1", Image = "css/picture/pic5.jpg", Details = "" },
+           new MovieViewModel { Id = 6, Name = "Captain America: Civil War", Rating = "1", Image = "css/picture/pic6.jpg", Details = "" },
+           new MovieViewModel { Id = 7, Name = "Doctor Strange", Rating = "1", Image = "css/picture/pic7.jpg", Details = "" },
+           new MovieViewModel { Id = 8, Name = "Spiderman Homecoming", Rating = "1", Image = "css/picture/pic8.jpg", Details = "" },
+           new MovieViewModel { Id = 9, Name = "Black Panther", Rating = "1", Image = "css/picture/pic9.jpg", Details = "" },
+           new MovieViewModel { Id = 10, Rating = "1", Image = "css/picture/pic10.jpg", Details = "" },
+           new MovieViewModel { Id = 11,Rating = "1", Image = "css/picture/pic11.jpg", Details = "" },
+           new MovieViewModel { Id = 12, Rating = "1", Image = "css/picture/pic12.jpg", Details = "" },
+           new MovieViewModel { Id = 13, Rating = "1", Image = "css/picture/pic13.jpg", Details = "" },
+           new MovieViewModel { Id = 14, Rating = "1", Image = "css/picture/pic14.jpg", Details = "" },
+           new MovieViewModel { Id = 15, Rating = "1", Image = "css/picture/pic15.jpg", Details = "" },
+
         };
     }
 
@@ -37,4 +51,3 @@ public class HomeController : Controller
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
-
--- BSIT3L-Movies/Models/MoviesModel.cs
+++ BSIT3L-Movies/Models/MoviesModel.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 namespace BSIT3L_Movies.Models
 {
-    
-	public class MovieViewModel
+
+    public class MovieViewModel
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string Rating { get; set; }
-        public int ReleaseYear { get; set; }
+        public string Image { get; set; }
+        public string Details { get; set; }
+
+        public string Description { get; set; }
         public string Genre { get; set; }
-        
+
+
+
     }
 }
-
